bdcbfec8067396f8cc80ab1d82d9497ec7847bc2
vladimirlisovskij <vladimir.lisovskij.2002@gmail.com>
2021-05-28 16:00:41 +0300

    small fix

--- app/src/main/java/com/example/myapplication/presenter/mainActivity/MainPresenter.kt
+++ app/src/main/java/com/example/myapplication/presenter/mainActivity/MainPresenter.kt
@@ -1,28 +1,9 @@
 package com.example.myapplication.presenter.mainActivity
 
-import com.example.myapplication.presenter.firstPage.FirstPage
-import com.example.myapplication.presenter.secondPage.SecondPage
-import com.example.myapplication.presenter.thirdPage.ThirdPageFragment
 import com.github.terrakok.cicerone.Router
-import com.github.terrakok.cicerone.androidx.FragmentScreen
 import moxy.InjectViewState
 import moxy.MvpPresenter
 import javax.inject.Inject
-
-
-object Screens {
-    val screen1 = FragmentScreen(){
-        FirstPage.newInstance()
-    }
-
-    val screen2 =  FragmentScreen {
-        SecondPage.newInstance()
-    }
-
-    val screen3 = FragmentScreen {
-        ThirdPageFragment.newInstance()
-    }
-}
 
 @InjectViewState
 class MainPresenter @Inject constructor(
--- app/src/main/java/com/example/myapplication/presenter/mainActivity/Screens.kt
+++ app/src/main/java/com/example/myapplication/presenter/mainActivity/Screens.kt
@@ -1,10 +1,21 @@
 package com.example.myapplication.presenter.mainActivity
 
-//
-//object Screens {
-//    fun First() {
-//        FragmentScreen {
-//            SecondPage()
-//        }
-//    }
-//}
+import com.example.myapplication.presenter.firstPage.FirstPage
+import com.example.myapplication.presenter.secondPage.SecondPage
+import com.example.myapplication.presenter.thirdPage.ThirdPageFragment
+import com.github.terrakok.cicerone.androidx.FragmentScreen
+
+
+object Screens {
+    val screen1 = FragmentScreen(){
+        FirstPage.newInstance()
+    }
+
+    val screen2 =  FragmentScreen {
+        SecondPage.newInstance()
+    }
+
+    val screen3 = FragmentScreen {
+        ThirdPageFragment.newInstance()
+    }
+}
